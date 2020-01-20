f55ad1cf5dfe699943f9861b0a3af4326b4ebc13
sleter <sleter97@gmail.com>
2020-01-20 21:26:20 +0100

    update scripts

---
+++ scripts/download_trec_eval.sh
@@ -0,0 +1,5 @@
+curl -O https://trec.nist.gov/trec_eval/trec_eval-9.0.7.tar.gz
+tar -zxvf trec_eval*.tar.gz
+rm -f trec_eval*.tar.gz
+cd trec_eval-9.0.7
+make
--- scripts/run_elasticsearch.sh
+++ scripts/run_elasticsearch.sh
@@ -1,5 +1,6 @@
 curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.1-linux-x86_64.tar.gz
 tar -zxvf elasticsearch-*.tar.gz
+rm -f elasticsearch-*.tar.gz
 cd elasticsearch-*/
 ./bin/elasticsearch
 https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.1-linux-x86_64.tar.gz
