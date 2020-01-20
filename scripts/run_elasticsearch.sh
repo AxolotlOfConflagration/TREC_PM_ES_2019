03b62987a39c27db5a1f2adad10bc521f347f82f
sleter <sleter97@gmail.com>
2020-01-20 15:32:28 +0100

    installation script for elasticsearch

---
+++ scripts/run_elasticsearch.sh
@@ -0,0 +1,5 @@
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.1-linux-x86_64.tar.gz
+tar -zxvf elasticsearch-*.tar.gz
+cd elasticsearch-*/
+./bin/elasticsearch
+https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.1-linux-x86_64.tar.gz
